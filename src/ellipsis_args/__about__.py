8d6a7d6b6a5fa7a74ceedec009fd274877a5c773
Nathaniel Blair-Stahn <ndbs@uw.edu>
2023-01-21 12:44:43 -0800

    make repo pip installable

---
+++ src/ellipsis_args/__about__.py
@@ -0,0 +1,22 @@
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "ellipsis_args"
+__summary__ = "A Python module for creating partially applied functions with flexible, intuitive syntax."
+__uri__ = "https://github.com/NathanielBlairStahn/ellipsis_args"
+
+__version__ = "0.0.1"
+
+__author__ = "Nathaniel Blair-Stahn"
+__email__ = "ndbs@uw.edu"
+
+__license__ = "BSD-3-Clause"
+__copyright__ = f"Copyright 2023 {__author__}"
