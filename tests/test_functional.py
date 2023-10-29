882414565c5d2f5d80ba84b25d84fe7625d60ee5
Nathaniel Blair-Stahn <ndbs@uw.edu>
2023-10-29 01:02:16 -0700

    add more tests

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,3 +1,4 @@
+import pytest
 from ellipsis_args.functional import partially_applicable
 
 @partially_applicable
@@ -14,7 +15,15 @@ def g(*args, **kwargs):
 
 def test_f_no_kwargs():
     assert f_no_kwargs(3, 5, 7) == 'a=3, b=5, c=7'
-    assert f_no_kwargs(..., 8, 4)(90) == 'a=90, b=8, c=4'
-    assert f_no_kwargs(5, ..., 12)(47) == 'a=5, b=47, c=12'
-    assert f_no_kwargs(3, ..., ...)(2, 8) == 'a=3, b=2, c=8'
-    assert f_no_kwargs(..., 3, ...)(2, 8) == 'a=2, b=3, c=8'
+    assert f_no_kwargs(..., 5, 7)(3) == 'a=3, b=5, c=7'
+    assert f_no_kwargs(3, ..., 7)(5) == 'a=3, b=5, c=7'
+    assert f_no_kwargs(3, ..., ...)(5, 7) == 'a=3, b=5, c=7'
+    assert f_no_kwargs(..., 5, ...)(3, 7) == 'a=3, b=5, c=7'
+    assert f_no_kwargs(..., ..., c=7)(3, 5) == 'a=3, b=5, c=7'
+    assert f_no_kwargs(..., b=5, c=7)(3) == 'a=3, b=5, c=7'
+    # Raises SyntaxError: positional argument follows keyword argument
+    # since ... comes after b=5. This happens on compilation, so can't run:
+    # assert f_no_kwargs(..., b=5, ...)(3, 7) == 'a=3, b=5, c=7'
+    assert f_no_kwargs(..., b=5, c=...)(3, c=7) == 'a=3, b=5, c=7'
+    # Currently fails but should pass:
+    # assert f_no_kwargs(..., b=5, c=...)(3, 7) == 'a=3, b=5, c=7'
