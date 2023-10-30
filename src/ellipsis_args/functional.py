bfd40bce5d472692c1eefa22ea492c9c42688f0e
Nathaniel Blair-Stahn <ndbs@uw.edu>
2023-10-30 00:03:15 -0700

    add logic to pass omitted kwd arguments as positional arguments and vice versa

--- src/ellipsis_args/functional.py
+++ src/ellipsis_args/functional.py
@@ -46,7 +46,8 @@ def partially_applicable_no_kwargs(func, nullary_as_const=True):
 # original positional arguments were.
 def partially_applicable(func, nullary_as_const=True):
     def partially_applicable_func(*args, **kwargs):
-        # This list maps the index of each new argument to its original position in args
+        # This list maps the index of each new argument
+        # to its original position in args
         omitted_indices = [j for j, arg in enumerate(args) if arg is SENTINEL]
         omitted_kwds = [kwd for kwd, val in kwargs.items() if val is SENTINEL]
         logger.debug(f"{omitted_indices=}, {omitted_kwds=}")
@@ -54,23 +55,48 @@ def partially_applicable(func, nullary_as_const=True):
         def partially_applied_func(*fewer_args, **fewer_kwargs):
             logger.debug(f"{omitted_indices=}, {args=}, {fewer_args=}")
             logger.debug(f"{omitted_kwds=}, {kwargs=}, {fewer_kwargs=}")
-            if (n_given:=len(fewer_args)+len(fewer_kwargs)) != (n_required:=len(omitted_indices)+len(omitted_kwds)):
+            # TODO: Figure out if there's a way to automatically
+            # generate a more specific error message by checking
+            # the passed arguments against the function signature.
+            # To do that, I'd need to figure out how to derive the
+            # correct signature given the original signature and
+            # the omitted arguments
+            if ((n_given:=len(fewer_args)+len(fewer_kwargs))
+                != (n_required:=len(omitted_indices)+len(omitted_kwds))):
                 raise TypeError(
                     f"The partially applied function takes {n_required} "
                     f"arguments, but {n_given} were given"
                 )
             new_args, new_kwargs = list(args), dict(kwargs)
 
-            for i,j in enumerate(omitted_indices):
-                new_args[j] = fewer_args[i]
-            for kwd in omitted_kwds:
-                new_kwargs[kwd] = fewer_kwargs[kwd]
+            for i, j in enumerate(omitted_indices):
+                logger.debug(f"omitted_indices: {i=}, {j=}")
+                new_args[j] = (
+                    fewer_args[i] if i < len(fewer_args)
+                    # FIXME: look up value in fewer_kwargs --
+                    # need name of omitted variable from function
+                    # signature (using inspect module)
+                    else ... # Look up value in fewer_kwargs
+                )
+            for i, kwd in enumerate(omitted_kwds):
+                logger.debug(f"omitted_kwds: {i=}, {kwd=}")
+                # An ommitted kwd might have been passed as a positional
+                # arg in fewer_args instead of as a keyword arg in
+                # fewer_kwargs, so check to avoid KeyError
+                new_kwargs[kwd] = (
+                    fewer_kwargs[kwd] if kwd in fewer_kwargs
+                    # TODO: Double-check reasoning to make sure
+                    # this index is guaranteed to be in bounds
+                    else fewer_args[len(omitted_indices)+i]
+                )
             logger.debug(f"{new_args=}, {new_kwargs=}")
             return func(*new_args, **new_kwargs)
 
         if len(omitted_indices) == 0 and len(omitted_kwds) == 0:
             # Base case: Don't recurse when result is nullary
-            return partially_applied_func() if nullary_as_const else partially_applied_func
+            return (partially_applied_func()
+                    if nullary_as_const
+                    else partially_applied_func)
         else:
             # If not nullary, use recursion to get another partially applicable function
             return partially_applicable(partially_applied_func)
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -25,8 +25,9 @@ def test_f_no_kwargs():
     # since ... comes after b=5. This happens on compilation, so can't run:
     # assert f_no_kwargs(..., b=5, ...)(3, 7) == 'a=3, b=5, c=7'
     assert f_no_kwargs(..., b=5, c=...)(3, c=7) == 'a=3, b=5, c=7'
-    # Currently fails but should pass:
-    # assert f_no_kwargs(..., b=5, c=...)(3, 7) == 'a=3, b=5, c=7'
+    # Pass missing kwd arg as positional arg:
+    assert f_no_kwargs(..., b=5, c=...)(3, 7) == 'a=3, b=5, c=7'
+    # Test recursion of partially_applicable:
     assert f_no_kwargs(..., ..., c=7)(..., 5)(3) == 'a=3, b=5, c=7'
     assert f_no_kwargs(..., 5, c=...)(..., c=7)(3) == 'a=3, b=5, c=7'
     assert f_no_kwargs(3, ..., ...)(5, ...)(7) == 'a=3, b=5, c=7'
